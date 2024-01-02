bc5cf1370f1d691b79c14c60dd983b64539b77ef
Roberto Núñez <Roberto>
2024-01-01 19:35:38 -0500

    First example of websockets using wss library

---
+++ src/app.ts
@@ -0,0 +1,28 @@
+import { WebSocketServer, WebSocket } from "ws";
+
+const wss = new WebSocketServer({ port: 3000 });
+
+wss.on("connection", function connection(ws) {
+  console.log("Client connected...");
+  ws.on("error", console.error);
+
+  ws.on("message", function message(data) {
+    const payload = JSON.stringify({
+      type: "custom-message",
+      payload: data.toString(),
+    });
+    // ws.send(JSON.stringify(payload));
+    // All the clients except itself
+    wss.clients.forEach(function each(client) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(payload, { binary: false });
+      }
+    });
+  });
+
+  ws.on("close", () => {
+    console.log("Client disconnected");
+  });
+});
+
+console.log("http://localhost:3000");
